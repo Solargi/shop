944b33d9761bc6b6b2824a8ec33f48f66386d65f
Solargi <giacomo.solaro@gmail.com>
2023-08-21 23:07:57 +0200

    spring initialization

---
+++ src/main/java/com/example/shop/ShopApplication.java
@@ -0,0 +1,15 @@
+package com.example.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+//exclude disable spring security for now to be removed
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
