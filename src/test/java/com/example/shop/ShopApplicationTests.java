9ee419edcc766eb4c4c4a243985cab1278b9a3b2
Solargi <giacomo.solaro@gmail.com>
2023-10-29 19:41:43 +0100

    changed ShopAllplicationTest to use embedded db for tests, change this when implementing test containers

--- src/test/java/com/example/shop/ShopApplicationTests.java
+++ src/test/java/com/example/shop/ShopApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.shop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//annotation to be removed/modified when implementing test containers .
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class ShopApplicationTests {
 
 	@Test
