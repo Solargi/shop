7e103f3aa0942b6e66e7dd05694a84b7821951a3
Solargi <giacomo.solaro@gmail.com>
2023-11-15 17:26:01 +0100

    configurated security to allow swagger doc

---
+++ src/main/java/com/example/shop/security/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.example.shop.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Value("${api.endpoint.base-url}")
+    String baseUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        //overwrite default authentication
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/api/v1/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "configuration/ui",
+                                "configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui.html/**",
+                                "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, this.baseUrl + "/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, this.baseUrl + "/users").hasAuthority("ROLE_admin")
+                        .requestMatchers(HttpMethod.POST, this.baseUrl + "/items").hasAuthority("ROLE_admin")
+                        .requestMatchers(HttpMethod.PUT, this.baseUrl + "/items").hasAuthority("ROLE_admin")
+                        .requestMatchers(HttpMethod.DELETE, this.baseUrl + "/items").hasAuthority("ROLE_admin")
+
+
+                        //everything else you need to be authenticated
+                        .anyRequest().authenticated()
+
+                )
+                .headers(headers -> headers.frameOptions().disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
