9cc2fd5fd84e6e4798fb56847e5185839c7d0f99
Solargi <giacomo.solaro@gmail.com>
2023-12-09 13:44:56 +0100

    added field userId to jwt token

--- src/main/java/com/example/shop/security/JwtProvider.java
+++ src/main/java/com/example/shop/security/JwtProvider.java
@@ -27,6 +27,9 @@ public class JwtProvider {
                 .map(grantedAuthority -> grantedAuthority.getAuthority())
                 .collect(Collectors.joining(" "));
 
+        //get user principal
+        UserPrincipal userPrincipal = (UserPrincipal)authentication.getPrincipal();
+
         //create jwt claims
         JwtClaimsSet claimsSet = JwtClaimsSet.builder()
                 .issuer("self") //means that we are not using a deticated authorization server, everything is done here
@@ -34,6 +37,7 @@ public class JwtProvider {
                 .expiresAt(now.plus(expiresIn, ChronoUnit.HOURS))
                 .subject(authentication.getName())
                 .claim("authorities", authorities)// custom defined claim
+                .claim("userId", userPrincipal.getUser().getId()) //add id attribute
                 .build();
         //encode the claims
         return this.jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
