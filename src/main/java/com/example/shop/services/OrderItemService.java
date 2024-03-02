37eb4365c4467b4057c2cd98b895ee425cff057d
Solargi <giacomo.solaro@gmail.com>
2024-03-02 14:58:33 +0100

    modified orderItemService to add or remove item available quantity when updating saving or deleting an orderItem

--- src/main/java/com/example/shop/services/OrderItemService.java
+++ src/main/java/com/example/shop/services/OrderItemService.java
@@ -7,6 +7,8 @@ import com.example.shop.models.OrderItem;
 import com.example.shop.repositories.ItemRepository;
 import com.example.shop.repositories.OrderItemRepository;
 import com.example.shop.repositories.OrderRepository;
+import com.example.shop.system.exceptions.GenericException;
+import com.example.shop.system.exceptions.ObjectAlreadyExistException;
 import com.example.shop.system.exceptions.ObjectNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -35,15 +37,21 @@ public class OrderItemService {
                 .orElseThrow(()->new ObjectNotFoundException("order", orderItem.getId().getOrderId()));
         Item item = this.itemRepository.findById(orderItem.getId().getItemId())
                 .orElseThrow(()->new ObjectNotFoundException("item", orderItem.getId().getItemId()));
-        orderItem.setItem(item);
-        orderItem.setOrder(order);
-        orderItem.setTotalCost(orderItem.computeTotalCost());
-        OrderItem savedOrderItem = this.orderItemRepository.save(orderItem);
-        //add savedOrderItem to current persitent instance of Order
-        order.addOrderItem(savedOrderItem);
-        //update order total
-        order.updateTotalCost();
-        return savedOrderItem;
+        Integer quantity = orderItem.getQuantity();
+        if (quantity > 0 && quantity <= item.getAvailableQuantity().intValue()){
+            updateItem(item.getId(),-quantity);
+            orderItem.setItem(item);
+            orderItem.setOrder(order);
+            orderItem.setTotalCost(orderItem.computeTotalCost());
+            OrderItem savedOrderItem = this.orderItemRepository.save(orderItem);
+            //add savedOrderItem to current persistent instance of Order
+            order.addOrderItem(savedOrderItem);
+            //update order total
+            order.updateTotalCost();
+            return savedOrderItem;
+        } else {
+            throw new GenericException("order quantity is higher than item's available quantity");
+        }
     }
 
     //only allowed update is quantity
@@ -52,20 +60,41 @@ public class OrderItemService {
         OrderItem oldOrderItem = this.orderItemRepository.findById(orderItemId)
                 .orElseThrow(()->new ObjectNotFoundException("orderItem",orderItemId));
 //        oldOrderItem.setOrder(update.getOrder());
-        oldOrderItem.setQuantity(update.getQuantity());
 //        oldOrderItem.setItem(update.getItem());
 //        oldOrderItem.setId(update.getId());
+        Integer quantityDifference = oldOrderItem.getQuantity() - update.getQuantity();
+        // if same quantity or more than original subtract difference form available quantity
+        //(update.getQuanitity can't be negative controlled in DTO)
+        if (quantityDifference <= 0){
+            updateItem(oldOrderItem.getId().getItemId(), quantityDifference);
+        }
+        // if quantity less than original add back difference to available quantity
+        if (quantityDifference > 0 && quantityDifference < oldOrderItem.getQuantity()){
+            updateItem(oldOrderItem.getId().getItemId(), quantityDifference);
+        }
+
+        oldOrderItem.setQuantity(update.getQuantity());
         oldOrderItem.setTotalCost(oldOrderItem.computeTotalCost());
         OrderItem savedOrderItem = this.orderItemRepository.save(oldOrderItem);
         //update order total price
-        savedOrderItem.updateTotalCost();
+        this.updateOrderCost(oldOrderItem.getId().getOrderId());
         return savedOrderItem;
     }
 
     public void delete(OrderItemId orderItemId){
         OrderItem orderItem = this.orderItemRepository.findById(orderItemId).orElseThrow(()-> new ObjectNotFoundException("orderItem",orderItemId));
+        Integer orderId = orderItem.getId().getOrderId();
+        Integer itemId = orderItem.getId().getItemId();
+        Integer quantity = orderItem.getQuantity();
         orderItem.removeFromOrder();
         this.orderItemRepository.deleteById(orderItemId);
+
+        //recompute order total cost would be better to set total cost as a computed property in the db
+        this.updateOrderCost(orderId);
+
+        //restore item availabe quantity
+        this.updateItem(itemId, quantity);
+
     }
 
 //    private void updateOrderTotalCost(OrderItem orderItem){
@@ -73,6 +102,20 @@ public class OrderItemService {
 //                .orElseThrow(() -> new ObjectNotFoundException("order", orderItem.getId().getOrderId()));
 //        order.updateTotalCost();
 //    }
+
+    private void updateItem (Integer itemId, Integer quantity){
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(()->new ObjectNotFoundException("item", itemId));
+        item.modifyQuantity(quantity);
+        this.itemRepository.save(item);
+
+    }
+    private void updateOrderCost(Integer orderId){
+        //recompute order total cost would be better to set total cost as a computed property in the db
+        Order order = this.orderRepository.findById(orderId).orElseThrow(()-> new ObjectNotFoundException("order",orderId));
+        order.setTotalCost(order.computeTotalCost());
+        this.orderRepository.save(order);
+    }
 
 
 
