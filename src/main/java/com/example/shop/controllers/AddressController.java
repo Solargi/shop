13ffa7b71175aca1f342dc568a06cd66d4489d60
Solargi <giacomo.solaro@gmail.com>
2024-03-02 14:53:52 +0100

    AddressController swagger documentation completed

--- src/main/java/com/example/shop/controllers/AddressController.java
+++ src/main/java/com/example/shop/controllers/AddressController.java
@@ -7,12 +7,11 @@ import com.example.shop.dtos.converters.AddressRequestDTOToAddressConverter;
 import com.example.shop.dtos.converters.AddressToAddressDTOConverter;
 import com.example.shop.dtos.converters.AddressDTOToAddressConverter;
 import com.example.shop.models.Address;
-import com.example.shop.models.User;
 import com.example.shop.services.AddressService;
-import com.example.shop.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -269,9 +268,9 @@ public class AddressController {
                 .map(this.addressToAddressDTOConverter::convert)
                 .toList());
     }
-
+    @SecurityRequirement(name = "bearerAuth")
     @Parameters({
-            @Parameter(name = "UserId", description = "The id the user that " +
+            @Parameter(name = "userId", description = "The id the user that " +
                     "will be associated with the new address", example = "1")
     })
     @Operation(
@@ -432,6 +431,171 @@ public class AddressController {
         return ResponseEntity.ok(this.addressToAddressDTOConverter.convert(savedAddress));
     }
 
+    @SecurityRequirement(name = "bearerAuth")
+    @Parameters({
+            @Parameter(name = "addressId", description = "The id of the target address", example = "1")
+    })
+    @Operation(
+            summary = "Update an existing address",
+            description = "an address can be updated and associated only to the currently logged in " +
+                    "user, unless the user has role admin, admins can update addresses associated to any user",
+
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AddressDTO.class),
+                            examples = {
+                                    @ExampleObject(name = "Address 1",
+                                            value = "{\n" +
+                                                    "  \"userDTO\": {\n" +
+                                                    "    \"id\": 1,\n" +
+                                                    "    \"username\": \"u1\",\n" +
+                                                    "    \"name\": \"a\",\n" +
+                                                    "    \"surname\": \"b\",\n" +
+                                                    "    \"email\": \"q@q.com\",\n" +
+                                                    "    \"birthDate\": \"3.1.1991\",\n" +
+                                                    "    \"roles\": \"admin\"\n" +
+                                                    "  },\n" +
+                                                    "  \"country\": \"Switzerland\",\n" +
+                                                    "  \"state\": \"Zurich\",\n" +
+                                                    "  \"city\": \"Zurich\",\n" +
+                                                    "  \"street\": \"Zurich strasse\",\n" +
+                                                    "  \"zipCode\": 8942\n" +
+                                                    "}",
+                                            description = "update the address associated with the " +
+                                                    "target id"
+                                    )
+                            }
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AddressDTO.class),
+                                    examples = {
+                                            @ExampleObject(name = "address data example",
+                                                    value = "{\n" +
+                                                            "  \"id\": 1,\n" +
+                                                            "  \"userDTO\": {\n" +
+                                                            "    \"id\": 1,\n" +
+                                                            "    \"username\": \"u1\",\n" +
+                                                            "    \"name\": \"a\",\n" +
+                                                            "    \"surname\": \"b\",\n" +
+                                                            "    \"email\": \"q@q.com\",\n" +
+                                                            "    \"birthDate\": \"3.1.1991\",\n" +
+                                                            "    \"roles\": \"admin\"\n" +
+                                                            "  },\n" +
+                                                            "  \"country\": \"Switzerland\",\n" +
+                                                            "  \"state\": \"Zurich\",\n" +
+                                                            "  \"city\": \"Zurich\",\n" +
+                                                            "  \"street\": \"Zurich strasse\",\n" +
+                                                            "  \"zipCode\": 8942\n" +
+                                                            "}",
+                                                    description = "address data"
+                                            )
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "missing or empty requiered fields",
+                                                    value = "{\n" +
+                                                            "    \"country\": \"must not be empty\",\n" +
+                                                            "    \"state\": \"must not be empty\",\n" +
+                                                            "    \"city\": \"must not be empty\",\n" +
+                                                            "    \"street\": \"must not be empty\",\n" +
+                                                            "    \"zipCode\": \"must not be empty\"" +
+                                                            "}",
+                                                    description = "(the response can either be empty or contain one or more of the listed elements)"
+                                            )
+
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(
+                                    mediaType = "*/*",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "unauthorized",
+                                                    description = "you will get an unauthorized response if you are not " +
+                                                            "logged in, meaning that " +
+                                                            "you didn't provide a JWT or you provided an invalid/expired one"
+                                            )
+
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(
+                                    mediaType = "*/*",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "fetching addresses while having role user",
+                                                    description = "trying to update the addresses using a JWT with role user, " +
+                                                            "will result in an empty forbidden response"
+                                            )
+
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(
+                                    mediaType = "text",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "user not found in database",
+                                                    value = "could not find user with id 1",
+                                                    description = "the user with the provided id does not exist"
+                                            ),
+                                            @ExampleObject(
+                                                    name = "address not found in database",
+                                                    value = "could not find address with id 1",
+                                                    description = "the address with the provided id does not exist"
+                                            )
+
+                                    }
+                            )
+                    ),
+                    @ApiResponse(responseCode = "500", description = "internal Server Error", content = {
+                            @Content(
+                                    mediaType = "text",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "some error",
+                                                    value = "either empty or a string with the error",
+                                                    description = "this response body can contain either the error text or be empty"
+                                            ),
+                                    }
+                            ),
+                            @Content(
+                                    mediaType = "*/*",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "empty",
+                                                    summary = "empty response"
+                                            ),
+                                    }
+                            )
+                    }
+                    )
+            }
+
+    )
     @PutMapping("/{addressId}")
     ResponseEntity<AddressDTO> updateAddress (@PathVariable Integer addressId, @Valid @RequestBody AddressDTO addressDTO){
         Address address = this.addressDTOToAddressConverter.convert(addressDTO);
@@ -439,6 +603,101 @@ public class AddressController {
         AddressDTO updatedAddressDTO = this.addressToAddressDTOConverter.convert(updatedAddress);
         return ResponseEntity.ok(updatedAddressDTO);
     }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @Parameters({
+            @Parameter(name = "addressId", description = "The id of the target address", example = "1",
+                    in = ParameterIn.PATH)
+    })
+    @Operation(
+            summary = "Delete Address",
+            description = "you can only delete the currently logged in user's address unless the logged in users has " +
+                    "role admin, admins can delete all addresses. trying to delete another user's address with an account having role user" +
+                    "will cause a 403 forbidden response.",
+
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = "*/*",
+                                    examples = {
+                                            @ExampleObject(name = "successful delete",
+                                                    value = "address deleted successfully!"
+                                            )
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(
+                                    mediaType = "*/*",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "not admin or unauthorized",
+                                                    description = "you will get an unauthorized response if you are not " +
+                                                            "logged in, meaning that " +
+                                                            "you didn't provide a JWT or you provided an invalid/expired one"
+                                            )
+
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(
+                                    mediaType = "*/*",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "deleting another user's address while having role user",
+                                                    description = "trying to delete another user's address using a JWT with role user, " +
+                                                            "will result in an empty forbidden response"
+                                            )
+
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(
+                                    mediaType = "text",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "address not found in database",
+                                                    value = "could not find address with id 1",
+                                                    description = "the user with the provided id does not exist"
+                                            )
+
+                                    }
+                            )
+                    ),
+                    @ApiResponse(responseCode = "500", description = "internal Server Error", content = {
+                            @Content(
+                                    mediaType = "text",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "some error",
+                                                    value = "either empty or a string with the error",
+                                                    description = "this response body can contain either the error text or be empty"
+                                            ),
+                                    }
+                            ),
+                            @Content(
+                                    mediaType = "*/*",
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "empty",
+                                                    summary = "empty response"
+                                            ),
+                                    }
+                            )
+                    }
+                    )
+            }
+    )
 
     @DeleteMapping("/{addressId}")
     public ResponseEntity<String> deleteAddress (@PathVariable Integer addressId){
