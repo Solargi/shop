c650740782edcc44b7c69ff84d789c857cbac74e
Solargi <giacomo.solaro@gmail.com>
2024-05-13 09:53:14 +0200

    added jwt also in cookie when login and added logut route (remove cookies from browser)

--- src/main/java/com/example/shop/security/AuthController.java
+++ src/main/java/com/example/shop/security/AuthController.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -16,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -24,6 +27,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.time.Duration;
 import java.util.Map;
 
 
@@ -71,10 +75,37 @@ public class AuthController {
     )
     @SecurityRequirement(name = "basicAuth")
     @PostMapping("/login")
-    // the authentication object is passed from springboot when login is successfull;
-    public ResponseEntity<Object> getLoginInfo (Authentication authentication){
+    // the authentication object is passed from springboot when login is successful;
+    public ResponseEntity<Object> getLoginInfo (Authentication authentication, HttpServletResponse response){
         LOGGER.debug("Authenticated user : '{}'", authentication.getName());
-        return ResponseEntity.ok(this.authService.createLoginInfo(authentication));
+        Map<String, Object> auth = this.authService.createLoginInfo(authentication);
+        //send jwt token in https only cookie
+        ResponseCookie cookie = ResponseCookie.from("token", (String) auth.get("token"))
+                //set secure to true in production so that the cookie is sent only if the connection is secure
+                //(it's using https)
+                .secure(false)
+                .httpOnly(true)
+                .sameSite("true")
+                .maxAge(Duration.ofHours(2))
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        //consider removing cookie from response if used in cookies this probably means changing tests as well
+        return ResponseEntity.ok(auth);
+    }
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping("/logout")
+    public ResponseEntity<Object> logout(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("token", null)
+                //set secure to true in production so that the cookie is sent only if the connection is secure
+                //(it's using https)
+                .secure(false)
+                .httpOnly(true)
+                .sameSite("true")
+                .maxAge(0)
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return ResponseEntity.ok().build();
     }
 
 
