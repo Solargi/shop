c962fc9448b204ac95eea97440aaa5404f1b33a6
Solargi <giacomo.solaro@gmail.com>
2024-06-04 13:21:23 +0200

    added button component draft

---
+++ shop-frontend/src/components/CButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button :class="['bg-button-light-600 dark:bg-button-dark-600',{ 'rounded-md':roundBorders, 'rounded-full':round, }]"> <Text>{{ props.text }}</Text> </button>
+</template>
+
+<script setup>
+import Text from "./Text.vue";
+
+
+const props = defineProps({
+    text: String,
+    roundBorders : Boolean,
+    round : Boolean,
+
+})
+
+</script>
