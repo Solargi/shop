67ff5a23a969426f7647606818c4646acd247417
Solargi <giacomo.solaro@gmail.com>
2024-07-08 13:20:10 +0200

    started working on order view

---
+++ shop-frontend/src/components/Order.vue
@@ -0,0 +1,25 @@
+<template>
+<Text bold class=" text-xl"> Order ID: {{ id }}</Text>
+<Text bold class=" text-xl"> Paid: {{ Paid }}</Text>
+<Text bold class=" text-xl"> Address: {{ shippingAddress }}</Text>
+<Text bold class=" text-xl"> Status: {{ status }}</Text>
+<Text bold class=" text-xl"> shippingCost: {{ shippingCost }}</Text>
+<Text bold class=" text-xl"> total: {{ totalCost }}</Text>
+
+
+</template>
+
+<script setup>
+import CartItemCard from './CartItemCard.vue'
+import Text from './Text.vue';
+
+const props = defineProps({
+    id: Number,
+    Paid: Boolean,
+    shippingAddress: String,
+    shppingCost: Number,
+    totalCost: Number,
+    status: String,
+
+})
+</script>
--- shop-frontend/src/views/OrdersView.vue
+++ shop-frontend/src/views/OrdersView.vue
@@ -1,11 +1,31 @@
 <template>
     <p>{{ api.data}}</p>
+    <Order></Order>
+    <div class="" v-for="(order, index) in api.data">
+          <Order class="max-w-7xl"
+            :id="order.id"
+            :paid="order.paid"
+            v-model:cost="order.totalCost"
+            :shippingAddress="order.shippingAddress"
+            v-model:shippingCost="order.shippingCOst"
+            :status="order.status"
+            @remove="api.data.splice(index, 1)"
+          ></Order>
+          <!-- <p>{{ order }}</p> -->
+        </div>
 </template>
-  
+id: Number,
+Paid: Boolean,
+shippingAddress: String,
+shppingCost: Number,
+totalCost: Number,
+status: String,
 <script setup>
 import useAxios from "@/composables/useAPI";
+import Order from "@/components/Order.vue"
 const api = useAxios();
 
-api.get("/orders")
+api.get("/orders/1")
+
 
 </script>
