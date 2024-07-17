79dd02e35f16ed05fe356773c2ffeda7ecc1daf7
Solargi <giacomo.solaro@gmail.com>
2024-07-17 12:54:07 +0200

    fixed token deletion issue added get orders by user endpoint and updated securityconfig

--- src/main/java/com/example/shop/repositories/OrderRepository.java
+++ src/main/java/com/example/shop/repositories/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.example.shop.repositories;
 
+import com.example.shop.models.CartItem;
 import com.example.shop.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository <Order, Integer> {
+import java.util.List;
 
+public interface OrderRepository extends JpaRepository <Order, Integer> {
+    public List<Order> findAllByUserId(Integer userId);
 }
